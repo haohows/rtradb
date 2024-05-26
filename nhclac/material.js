445a17962c680137109b5bf56268833bf43bd2e9
黃阿皓 <faraway520@gmail.com>
2024-05-26 10:35:12 +0800

    更新

--- nhclac/material.js
+++ nhclac/material.js
@@ -5,7 +5,11 @@ const nhclac = {
     { file: "nhclac/image/cat1.webp", name: "cat1" },
     { file: "nhclac/image/cat2.webp", name: "cat2" },
     { file: "nhclac/image/cat3.webp", name: "cat3" },
-    { file: "nhclac/image/taskbanner.webp", name: "taskbanner" }
+    { file: "nhclac/image/taskbanner.webp", name: "taskbanner" },
+    { file: "nhclac/image/evtbg.webp", name: "evtbg" },
+    { file: "nhclac/image/evtitem01.webp", name: "evtitem01" },
+    { file: "nhclac/image/evtitem02.webp", name: "evtitem02" },
+    { file: "nhclac/image/evtitem03.webp", name: "evtitem03" },
   ],
   armark: [],
   armodel: [],
