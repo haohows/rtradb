92d8c5fded722ddb17becaf47b0e6baa0d8eb68f
黃阿皓 <faraway520@gmail.com>
2024-06-23 23:06:19 +0800

    更新

---
+++ mbook/material.js
@@ -0,0 +1,16 @@
+const mbook = {
+  image: [
+    { file: "mbook/image/stu.webp", name: "stu" },
+    { file: "mbook/image/elf.webp", name: "elf" },
+    { file: "mbook/image/holyarea.webp", name: "holyarea" },
+    { file: "mbook/image/lumcard.webp", name: "lumcard" },
+  ],
+  armark: [],
+  armodel: [],
+  music: [],
+  video: [],
+};
+
+window.setMbook = () => {
+  return mbook;
+};
