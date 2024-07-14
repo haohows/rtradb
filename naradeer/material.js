0aed5543ce8ded87c894df89e611fc51253c200d
黃阿皓 <faraway520@gmail.com>
2024-07-14 22:06:51 +0800

    更新

---
+++ naradeer/material.js
@@ -0,0 +1,26 @@
+const naradeer = {
+  image: [
+    { file: "naradeer/image/role/smile.webp", name: "smile" },
+    { file: "naradeer/image/role/goodmood.webp", name: "goodmood" },
+    { file: "naradeer/image/role/surprise.webp", name: "surprise" },
+    { file: "naradeer/image/role/normal.webp", name: "normal" },
+    { file: "naradeer/image/role/think.webp", name: "think" },
+    { file: "naradeer/image/role/laugh.webp", name: "laugh" },
+    { file: "naradeer/image/role/rest.webp", name: "rest" },
+    { file: "naradeer/image/role/bewilder.webp", name: "bewilder" },
+    { file: "naradeer/image/role/confuse.webp", name: "confuse" },
+    { file: "naradeer/image/role/deer.webp", name: "deer" },
+    { file: "naradeer/image/taskImg/deer.webp", name: "deer" },
+    { file: "naradeer/image/taskImg/smoke.webp", name: "smoke" },
+    { file: "naradeer/image/taskImg/transform.webp", name: "transform" },
+    { file: "naradeer/image/taskImg/toriistreet.webp", name: "toriistreet" },
+    { file: "naradeer/image/evtbg/street.webp", name: "street" },
+  ],
+  armark: [
+
+  ],
+};
+
+window.setNaradeer = () => {
+  return naradeer;
+};
