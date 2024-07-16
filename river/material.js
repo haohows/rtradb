935f87700eb02ad5de6cb5a0d9532f0be6b1ff15
黃阿皓 <faraway520@gmail.com>
2024-07-17 06:34:53 +0800

    更新

--- river/material.js
+++ river/material.js
@@ -1,6 +1,7 @@
 const river = {
   image: [
     { file: "river/image/taskImg/cover.webp", name: "cover" },
+    { file: "river/image/taskImg/unknown.webp", name: "unknown" },
     { file: "river/image/clueobj/brick.webp", name: "brick" },
   ],
   armark: [],
