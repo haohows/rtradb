cd9c4f4e5e34806ed37a2c78e3e807210908c967
黃阿皓 <faraway520@gmail.com>
2024-07-18 05:12:35 +0800

    更新

--- river/material.js
+++ river/material.js
@@ -4,8 +4,13 @@ const river = {
     { file: "river/image/taskImg/unknown.webp", name: "unknown" },
     { file: "river/image/clueobj/brick.webp", name: "brick" },
     { file: "river/image/evtbg/bgblack.webp", name: "bgblack" },
+    { file: "river/image/arimg/flower.webp", name: "flower" },
+    { file: "river/image/arimg/tree.webp", name: "tree" },
   ],
-  armark: [],
+  armark: [
+    { file: "river/armark/flower.mind", name: "flower" },
+    { file: "river/armark/tree.mind", name: "tree" },
+  ],
   armodel: [],
   music: [
     { file: "river/music/rivergod01.MP3", name: "rivergod01" },
