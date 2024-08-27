f7242883f96dd269ba81205f76d5f36d93d38de3
黃阿皓 <faraway520@gmail.com>
2024-08-28 03:39:56 +0800

    更新

--- river/material.js
+++ river/material.js
@@ -3,22 +3,47 @@ const river = {
     // 用戶照片
     { file: "river/image/design/clientphoto.webp", name: "clientphoto" },
 
+    // 角色
+    { file: "river/image/role/dragon.webp", name: "dragon" },
+    { file: "river/image/role/fish.webp", name: "fish" },
+    { file: "river/image/role/kirin.webp", name: "kirin" },
+    { file: "river/image/role/peony.webp", name: "peony" },
+    { file: "river/image/role/tree.webp", name: "tree" },
+
+    // 背景
+    { file: "river/image/evtbg/bgblack.webp", name: "bgblack" },
+
+    // 任務
     { file: "river/image/taskImg/cover.webp", name: "cover" },
     { file: "river/image/taskImg/unknown.webp", name: "unknown" },
-    { file: "river/image/clueobj/brick.webp", name: "brick" },
-    { file: "river/image/evtbg/bgblack.webp", name: "bgblack" },
+    { file: "river/image/taskImg/floortiles.webp", name: "floortiles" },
+    { file: "river/image/taskImg/steamtrain.webp", name: "steamtrain" },
+
+    // 線索
+    { file: "river/image/clueobj/floortiles.webp", name: "floortiles" },
+    { file: "river/image/clueobj/chaojiangpoetry.webp", name: "chaojiangpoetry" },
+    { file: "river/image/clueobj/ciyoupoetry.webp", name: "ciyoupoetry" },
+    { file: "river/image/clueobj/fordpoetry.webp", name: "fordpoetry" },
+    { file: "river/image/clueobj/guangfupoetry.webp", name: "guangfupoetry" },
+    { file: "river/image/clueobj/treepoetry.webp", name: "treepoetry" },
+    { file: "river/image/clueobj/wenchangpoetry.webp", name: "wenchangpoetry" },
+
+    // 標記
     { file: "river/image/arimg/flower.webp", name: "flower" },
     { file: "river/image/arimg/tree.webp", name: "tree" },
     { file: "river/image/arimg/fire.webp", name: "fire" },
   ],
+
   armark: [
-    { file: "river/armark/flowertree.mind", name: "flowertree" },
-    { file: "river/armark/fire.mind", name: "fire" },
+    { file: "river/armark/chaojiang.mind", name: "chaojiang" },
+    { file: "river/armark/ciyoupalace.mind", name: "ciyoupalace" },
+    { file: "river/armark/ford.mind", name: "ford" },
+    { file: "river/armark/guangfu.mind", name: "guangfu" },
+    { file: "river/armark/wenchang.mind", name: "wenchang" },
+    { file: "river/armark/temple.mind", name: "temple" },
   ],
   armodel: [],
-  music: [
-    { file: "river/music/rivergod01.MP3", name: "rivergod01" },
-  ],
+  music: [{ file: "river/music/rivergod01.MP3", name: "rivergod01" }],
   video: [],
 };
 
