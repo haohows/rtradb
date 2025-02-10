4822b913f9ce3afb589b787add4ae1f5d289025a
黃阿皓 <faraway520@gmail.com>
2025-02-11 03:07:27 +0800

    更新

--- basesetting/material.js
+++ basesetting/material.js
@@ -1,15 +1,5 @@
 const baseSet = {
-  image: [
-    { file: "basesetting/image/avatar.webp", name: "品牌LOGO" },
-    { file: "basesetting/image/unkChar.webp", name: "未知人物" },
-    { file: "basesetting/image/taskBg.webp", name: "任務黑幕" },
-    { file: "basesetting/image/bgShade.webp", name: "背景黑幕" },
-    { file: "basesetting/image/mirror-s.png", name: "春日鏡" },
-    { file: "basesetting/image/mirror-m.png", name: "春日鏡" },
-    { file: "basesetting/image/mirror-l.png", name: "春日鏡" },
-    { file: "basesetting/image/mirror3.png", name: "春日鏡" },
-    { file: "basesetting/image/mirrorgod.png", name: "春日之神" },
-  ],
+  image: [{ file: "basesetting/image/avatar.webp", name: "品牌LOGO" }],
   armark: [],
   armodel: [],
   music: [],
---
+++ vanish/material.js
@@ -0,0 +1,16 @@
+const vanish = {
+  image: [
+    { file: "vanish/image/role/perfumer.webp", name: "perfumer" },
+  
+    { file: "vanish/image/taskImg/toriistreet.webp", name: "toriistreet" },
+    { file: "vanish/image/evtbg/street.webp", name: "street" },
+  ],
+  armark: [],
+  armodel: [],
+  music: [],
+  video: [],
+};
+
+window.setvanish = () => {
+  return vanish;
+};
