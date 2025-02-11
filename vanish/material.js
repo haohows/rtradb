0b108f8a917f19091c1a41e7223f59c1ef2a9b80
Hao <faraway520@gmail.com>
2025-02-11 09:00:15 +0800

    更新

--- vanish/material.js
+++ vanish/material.js
@@ -1,9 +1,10 @@
 const vanish = {
   image: [
     { file: "vanish/image/role/perfumer.webp", name: "perfumer" },
-  
     { file: "vanish/image/taskImg/toriistreet.webp", name: "toriistreet" },
     { file: "vanish/image/evtbg/street.webp", name: "street" },
+    { file: "vanish/image/clueobj/timeGuide.webp", name: "timeGuide" },
+    { file: "vanish/image/clueobj/rebirthRiddle.webp", name: "rebirthRiddle" },
   ],
   armark: [],
   armodel: [],
