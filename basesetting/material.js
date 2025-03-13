5ee5bdb6974b68a6f23d2882d7fa9e371f056880
黃阿皓 <faraway520@gmail.com>
2025-03-14 03:52:38 +0800

    更新

--- basesetting/material.js
+++ basesetting/material.js
@@ -1,6 +1,12 @@
 const baseSet = {
-  image: [{ file: "basesetting/image/avatar.webp", name: "品牌LOGO" }],
-  armark: [],
+  image: [
+    { file: "basesetting/image/avatar.webp", name: "品牌LOGO" },
+    { file: "basesetting/image/tiger.webp", name: "tiger" },
+    { file: "basesetting/image/whale.webp", name: "whale" },
+  ],
+  armark: [
+    { file: "basesetting/armark/aropt.mind", name: "aropt" },
+  ],
   armodel: [],
   music: [],
   video: [],
