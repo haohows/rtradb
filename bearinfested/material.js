13f62afbd62eae8ce0a6877a57301f6c61cef8b9
黃阿皓 <faraway520@gmail.com>
2025-05-31 11:37:14 +0800

    更新-熊出沒

---
+++ bearinfested/material.js
@@ -0,0 +1,15 @@
+const bearinfested = {
+  image: [
+    { file: "bearinfested/image/role/smile.webp", name: "smile" },
+    { file: "bearinfested/image/evtbg/taipei101.webp", name: "taipei101" },
+    { file: "bearinfested/image/taskimg/coastaltown.webp", name: "coastaltown" },
+  ],
+  armark: [],
+  armodel: [],
+  music: [],
+  video: [],
+};
+
+window.setBearinfested = () => {
+  return bearinfested;
+};
