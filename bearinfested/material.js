5957c7c38bf5a9ec00ebce0e0091c86ab9c2afd8
黃阿皓 <faraway520@gmail.com>
2025-06-01 18:58:15 +0800

    更新

--- bearinfested/material.js
+++ bearinfested/material.js
@@ -3,6 +3,7 @@ const bearinfested = {
     { file: "bearinfested/image/role/smile.webp", name: "smile" },
     { file: "bearinfested/image/evtbg/taipei101.webp", name: "taipei101" },
     { file: "bearinfested/image/taskimg/coastaltown.webp", name: "coastaltown" },
+    { file: "bearinfested/image/objimg/subway.webp", name: "subway" },
   ],
   armark: [],
   armodel: [],
