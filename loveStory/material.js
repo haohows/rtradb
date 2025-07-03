d189cddf60a3e3185ac3393ed614873f1583a20c
Hao <faraway520@gmail.com>
2025-07-03 14:41:28 +0800

    更新

--- loveStory/material.js
+++ loveStory/material.js
@@ -5,6 +5,7 @@ const loveStory = {
     { file: "loveStory/image/evtbg/meeting.webp", name: "meeting" },
     { file: "loveStory/image/evtbg/runaway.webp", name: "runaway" },
     { file: "loveStory/image/evtbg/speech.webp", name: "speech" },
+    { file: "loveStory/image/evtbg/evtbg02.webp", name: "evtbg02" },
 
     { file: "loveStory/image/role/hero.webp", name: "hero" },
     { file: "loveStory/image/role/heroine.webp", name: "heroine" },
@@ -12,6 +13,8 @@ const loveStory = {
 
     { file: "loveStory/image/taskimg/wedding.webp", name: "wedding" },
     { file: "loveStory/image/itembg/itembg.webp", name: "itembg" },
+    { file: "loveStory/image/itembg/itembg02.webp", name: "itembg02" },
+    { file: "loveStory/image/itembg/itembg03.webp", name: "itembg03" },
   ],
   armark: [],
   armodel: [],
