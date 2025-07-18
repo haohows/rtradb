ea06a0920d8aeb626302abdd8b205499e10feb7d
黃阿皓 <faraway520@gmail.com>
2025-07-18 11:13:59 +0800

    更新

--- bearinfested/material.js
+++ bearinfested/material.js
@@ -103,6 +103,9 @@ const bearinfested = {
     { file: "bearinfested/image/arimg/imperialseal04.webp", name: "imperialseal04" },
     { file: "bearinfested/image/arimg/deepshrine.webp", name: "deepshrine" },
     { file: "bearinfested/image/itembg/itembg01.webp", name: "itembg01" },
+
+    { file: "bearinfested/image/guidelines/guidelines01.webp", name: "guidelines01" },
+    { file: "bearinfested/image/guidelines/guidelines02.webp", name: "guidelines02" },
   ],
   armark: [
     { file: "bearinfested/armark/zhuyin.mind", name: "zhuyin" },
