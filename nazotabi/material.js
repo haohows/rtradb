fa028a954634edd151fea4b772e49de125698d57
黃阿皓 <faraway520@gmail.com>
2025-09-29 12:17:23 +0800

    更新

--- nazotabi/material.js
+++ nazotabi/material.js
@@ -1,5 +1,11 @@
 const nazotabi = {
-  image: [{ file: "nazotabi/image/task/task01.webp", name: "task01" }],
+  image: [
+    { file: "nazotabi/image/task/task01.webp", name: "task01" },
+    { file: "nazotabi/image/task/task02.webp", name: "task02" },
+    { file: "nazotabi/image/task/task03.webp", name: "task03" },
+    { file: "nazotabi/image/task/task04.webp", name: "task04" },
+    { file: "nazotabi/image/task/task05.webp", name: "task05" },
+    ],
   armark: [],
   armodel: [],
   music: [
