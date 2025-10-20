b5f902cf97f51abac59da5bc41cfbeed4c2adc5b
黃阿皓 <faraway520@gmail.com>
2025-10-21 06:49:30 +0800

    更新

--- bearinfested/material.js
+++ bearinfested/material.js
@@ -93,6 +93,7 @@ const bearinfested = {
     { file: "bearinfested/image/evtbg/bg38.webp", name: "bg38" },
     { file: "bearinfested/image/evtbg/bg39.webp", name: "bg39" },
     { file: "bearinfested/image/evtbg/bg40.webp", name: "bg40" },
+    { file: "bearinfested/image/evtbg/bg41.webp", name: "bg41" },
 
     { file: "bearinfested/image/taskimg/task01.webp", name: "task01" },
     { file: "bearinfested/image/taskimg/task02.webp", name: "task02" },
