c0b4153569e994331402010555be03ccb707d9ba
黃阿皓 <faraway520@gmail.com>
2025-10-24 10:51:57 +0800

    更新

--- nazotabi/material.js
+++ nazotabi/material.js
@@ -38,6 +38,7 @@ const nazotabi = {
     { file: "nazotabi/image/role/bear15.webp", name: "bear15" },
     { file: "nazotabi/image/role/bear16.webp", name: "bear16" },
     { file: "nazotabi/image/role/bear17.webp", name: "bear17" },
+    { file: "nazotabi/image/role/bear18.webp", name: "bear18" },
 
     { file: "nazotabi/image/role/miko01.webp", name: "miko01" },
     { file: "nazotabi/image/role/miko02.webp", name: "miko02" },
