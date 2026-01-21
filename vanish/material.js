3489559b57e48203c66cb1f7cf9e674568d7042c
黃阿皓 <faraway520@gmail.com>
2026-01-22 06:58:59 +0800

    更新

--- vanish/material.js
+++ vanish/material.js
@@ -1,7 +1,19 @@
 const vanish = {
   image: [
     { file: "vanish/image/task/task01.webp", name: "task01" },
+    { file: "vanish/image/task/task02.webp", name: "task02" },
+
     { file: "vanish/image/evtbg/evtbg01.webp", name: "evtbg01" },
+    { file: "vanish/image/evtbg/evtbg02.webp", name: "evtbg02" },
+
+    { file: "vanish/image/evtbg/quest01.webp", name: "quest01" },
+    { file: "vanish/image/evtbg/quest02.webp", name: "quest02" },
+
+    { file: "vanish/image/evtbg/dire01.webp", name: "dire01" },
+    { file: "vanish/image/evtbg/dire02.webp", name: "dire02" },
+    { file: "vanish/image/evtbg/dire03.webp", name: "dire03" },
+    { file: "vanish/image/evtbg/dire04.webp", name: "dire04" },
+    { file: "vanish/image/evtbg/dire05.webp", name: "dire05" },
   ],
   armark: [],
   armodel: [],
